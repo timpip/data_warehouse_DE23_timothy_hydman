d06705b5b3c98c590ae7e1ac024fe329d1acc783
timpip <timothy.hydman@hotmail.com>
2024-09-02 16:25:51 +0200

    updates

---
+++ 12_dbt_testing/dbt_code/models/src/src_job_ads.sql
@@ -0,0 +1,9 @@
+WITH stg_job_ads AS (SELECT * FROM {{ source('job_ads', 'stg_data_ads') }})
+
+SELECT id,
+       headline,
+       number_of_vacancies as vacancies,
+       relevance,
+       application_deadline
+
+FROM stg_job_ads ORDER BY application_deadline ASC
